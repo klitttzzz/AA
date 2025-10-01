ec61c6125ee9397f5088b5409c52a2737af4f6af
Pabliitooo <pabloplazagarrido2006@gmail.com>
2025-10-01 18:13:45 +0200

    InsertSort

--- sorting.c
+++ sorting.c
@@ -19,11 +19,20 @@
 /***************************************************/
 int InsertSort(int* array, int ip, int iu)
 {
-int i = 0;
-int j = 0;
-for(i = ip; i < iu; i++) {
-  
+    if(!array || ip > iu) return ERR;
+int i, j, key;
+int num = 0;
+for (i = ip + 1; i <= iu; i++) {
+    key = array[i];
+    j = i - 1;
+    while(j >=ip && array[j] > key) {
+        swap_(&array[j], &array[j + 1]);
+        j--;
+        num++;
+    }
 }
+
+return num;
 
 
 }
@@ -36,6 +45,12 @@ for(i = ip; i < iu; i++) {
 int BubbleSort(int* array, int ip, int iu)
 {
   
+}
+
+void swap_(int *orig, int *dest) {
+    int aux = *orig;
+    *orig = *dest;
+    *dest = aux;
 }
 
 
